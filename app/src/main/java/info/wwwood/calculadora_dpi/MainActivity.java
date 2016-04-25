855086b5b6c6ea7b15f0961da8454c80713172d1
Prog. Apps Android L y M de 15:30 a 20:30 <android-ed1@aula.local>
2016-04-25 19:55:14 +0200

    Classe 25/04/2016

--- app/src/main/java/info/wwwood/calculadora_dpi/MainActivity.java
+++ app/src/main/java/info/wwwood/calculadora_dpi/MainActivity.java
@@ -1,8 +1,14 @@
 package info.wwwood.calculadora_dpi;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.OrientationEventListener;
+import android.view.Surface;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,22 +20,30 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText ET_ResolucionVertical,ET_ResolucionHorizontal,ET_Pulgadas;
+    private TextView TV_DPI_Value;
+    private Button BT_Calcular;
+    private Display display;
+
+    private TextView main_LAND_TV_ResolucionVertical,main_LAND_TV_RV,main_LAND_TV_RH,main_LAND_TV_ResolucionHorizontal,main_LAND_TV_Pulgadas,main_LAND_TV_DPI_Value;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display=((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
 
-        Button BT_Calcular = (Button) findViewById((R.id.BT_Calcular));
-        BT_Calcular.setOnClickListener(this); //IMPLEMENTEM CONTROLADORA DE CLICKS
-
+        if (display.getRotation()== Surface.ROTATION_0 || display.getRotation()== Surface.ROTATION_180){
+            //MODO VERTICAL
+            modoVertical();
+        } else {
+            modoHorizontal();
+        }
     }
 
     @Override
     public void onClick(View v) {
-        EditText ET_ResolucionVertical = (EditText) findViewById(R.id.ET_ResolucionVertical);
-        EditText ET_ResolucionHorizontal = (EditText) findViewById(R.id.ET_ResolucionHorizontal);
-        EditText ET_Pulgadas = (EditText) findViewById(R.id.ET_Pulgadas);
-        TextView TV_DPI_Value=(TextView) findViewById(R.id.TV_DPI_Value);
+
 
         switch (v.getId()){
             case R.id.BT_Calcular:
@@ -39,9 +53,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Double H = Double.parseDouble(ET_ResolucionVertical.getText().toString());
                     Double W = Double.parseDouble(ET_ResolucionHorizontal.getText().toString());
                     Double pulgadas = Double.parseDouble(ET_Pulgadas.getText().toString());
-                    Double diagonal = Math.sqrt(Math.pow(W, 2) + (Math.pow(H, 2)));
-                    Double Resultat = roundTwoDecimals(diagonal / pulgadas);
-                    TV_DPI_Value.setText(Resultat.toString());
+                    //Double diagonal = Math.sqrt(Math.pow(W, 2) + (Math.pow(H, 2)));
+                    Double diagonal=Math.hypot(H,W);
+                    Double Resultat = (diagonal / pulgadas);
+                    //TV_DPI_Value.setText(Resultat.toString());
+                    TV_DPI_Value.setText(String.format("%.2f",Resultat));
                 }
                 break;
 
@@ -52,5 +68,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         DecimalFormat twoDForm = new DecimalFormat("#.##");
         return Double.valueOf(twoDForm.format(d));
+    }
+    private void modoVertical(){
+        ET_ResolucionVertical = (EditText) findViewById(R.id.ET_ResolucionVertical);
+        ET_ResolucionHorizontal = (EditText) findViewById(R.id.ET_ResolucionHorizontal);
+        ET_Pulgadas = (EditText) findViewById(R.id.ET_Pulgadas);
+        TV_DPI_Value=(TextView) findViewById(R.id.TV_DPI_Value);
+
+        BT_Calcular = (Button) findViewById((R.id.BT_Calcular));
+        BT_Calcular.setOnClickListener(this); //IMPLEMENTEM CONTROLADORA DE CLICKS
+
+    }
+    private void modoHorizontal(){
+        main_LAND_TV_ResolucionVertical = (TextView) findViewById(R.id.main_LAND_TV_ResolucionVertical);
+        main_LAND_TV_ResolucionHorizontal = (TextView) findViewById(R.id.main_LAND_TV_ResolucionHorizontal);
+        main_LAND_TV_Pulgadas = (TextView) findViewById(R.id.main_LAND_TV_Pulgadas);
+        main_LAND_TV_DPI_Value=(TextView) findViewById(R.id.main_LAND_TV_DPI_Value);
+
+
+        DisplayMetrics displayMetrics=new DisplayMetrics();
+
+        display.getMetrics(displayMetrics);
+        display.getRealMetrics(displayMetrics); //fa el mateix que getMetrics però real
+
+        main_LAND_TV_ResolucionVertical.setText(String.valueOf(displayMetrics.heightPixels));
+        main_LAND_TV_ResolucionHorizontal.setText(String.valueOf(displayMetrics.widthPixels));
+        main_LAND_TV_DPI_Value.setText(String.valueOf(displayMetrics.densityDpi));
+
+        main_LAND_TV_Pulgadas.setText(String.valueOf((Math.hypot(displayMetrics.heightPixels,displayMetrics.widthPixels)/displayMetrics.densityDpi)));
+
+
+
+
+
     }
 }
